5743b1ae0fb7ed8a70e898a9ac220d175e3a669a
Caitlin <caitlinsweeneylmt@gmail.com>
2018-05-01 20:33:28 -0700

    latest debugging file reader and tests

--- lab-3-sweeney/lib/read.js
+++ lab-3-sweeney/lib/read.js
@@ -5,56 +5,62 @@ const inputFiles = [
   '../assets/file2.txt',
   '../assets/file3.txt'
 ];
- 
-const fs = require('fs');
+let Reader = {};
 
-function readCb(err,results){ 
-  if(err){
-    console.log('there was an error reading text file');
+Reader.readFiles = (paths, callback) => {
+
+  if((paths === null ) || (paths === undefined)){
+    return undefined;
+  }
+ 
+  const fs = require('fs');
+
+  function readCb(err,results){ 
+    if(err){
+      console.log('there was an error reading text file');
+    };
+
+    let file = results.toString();
+
+    outputFile.push(file);
+    console.log('file:',file);
   };
 
-  let file = results.toString();
 
-  outputFile.push(file);
-  console.log('file:',file);
+  const outputFiles = [];
+
+  function completedAll(results, err) {
+    console.log('completed all');
+    if (outputFiles.length !== null){
+      return outputFiles;
+    }
+    else return err;
+  }
+
+  let delay = 10000;
+  const arrayOfPromises = Object.keys(paths).map(()=> (
+    new Promise((resolve, reject) => {
+      const content = fs.readFile(index);
+      console.log('request', content);
+      setTimeout((err, content) => {
+        if (err) {
+          reject(err);
+        }
+
+        // Some code for which I use object values
+        resolve(content);
+      }, (delay /= 2));
+    }))
+    .then((content, err) => readCb(content, err)));
+
+  Promise.all(arrayOfPromises)
+    .then(completedAll)
+    .catch(console.error);
+
+
 };
 
-
-const outputFiles = [];
-
-function completedAll(results,err) {
-  console.log('completed all');
-  if (outputFiles.length !== null){
-    return outputFiles;
-  }
-  else return err;
-}
-
-let delay = 10000;
-const arrayOfPromises = Object.keys(inputFiles).map(()=> (
-  new Promise((resolve, reject) => {
-    const content = fs.readFile(inputFiles[index]);
-    console.log('request', content);
-    setTimeout((err, content) => {
-      if (err) {
-        reject(err);
-      }
-
-      // Some code for which I use object values
-      resolve(content);
-    }, (delay /= 2));
-  }))
-  .then(readCb));
-
-Promise.all(arrayOfPromises)
-  .then(completedAll)
-  .catch(console.error);
-
-
-
-
-
-module.exports.Readthreefiles;
+module.exports = Reader;
 
 
 
